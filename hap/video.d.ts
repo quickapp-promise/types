5902353f6e00470378991797fa6452390056d126
qinjiapeng <759848795@qq.com>
2025-02-19 16:29:21 +0800

    update @hap.io.Video

--- hap/video.d.ts
+++ hap/video.d.ts
@@ -3,115 +3,220 @@
  * 接口声明: { "name": "hap.io.Video" }
  */
 declare module "@hap.io.Video" {
-  // 定义创建视频压缩任务实例的参数类型
   interface VideoCreationOptions {
-    /** 原视频文件地址，必填 */
+    /**
+     * 原视频文件地址，必填
+     */
     uri: string;
-    /** 压缩后视频的高，单位像素，默认原视频高。部分视频压缩后实际高度会向上对齐16位，原视频旋转角度为90或270时，压缩后实际宽高会和所设宽高互换，可选 */
+    /**
+     * 压缩后视频的高，单位像素，默认原视频高。
+     * 
+     * 部分视频压缩后实际高度会向上对齐16位，原视频旋转角度为90或270时，压缩后实际宽高会和所设宽高互换，可选
+     */
     height?: number;
-    /** 压缩后视频的宽，单位像素，默认原视频宽。部分视频压缩后实际宽度会向上对齐16位，原视频旋转角度为90或270时，压缩后实际宽高会和所设宽高互换，可选 */
+    /**
+     * 压缩后视频的宽，单位像素，默认原视频宽。
+     * 
+     * 部分视频压缩后实际宽度会向上对齐16位，原视频旋转角度为90或270时，压缩后实际宽高会和所设宽高互换，可选
+     */
     width?: number;
-    /** 压缩后视频的码率，单位bps(比特/秒)，默认原视频码率的1/2。受硬件平台限制，无法精确控制实际压缩后的码率与所设定码率一致，可选 */
+    /**
+     * 压缩后视频的码率，单位bps(比特/秒)，默认原视频码率的1/2。
+     * 
+     * 受硬件平台限制，无法精确控制实际压缩后的码率与所设定码率一致，可选
+     */
     bitrate?: number;
-    /** 压缩后视频的帧率，单位fps(帧/秒)，默认原视频帧率，若获取不到原视频帧率，则默认为30。受硬件平台限制，无法精确控制实际压缩后的帧率与所设定帧率一致，可选 */
+    /**
+     * 压缩后视频的帧率，单位fps(帧/秒)，默认原视频帧率，若获取不到原视频帧率，则默认为30。
+     * 
+     * 受硬件平台限制，无法精确控制实际压缩后的帧率与所设定帧率一致，可选
+     */
     framerate?: number;
   }
 
-  // 定义压缩进度监听回调函数的返回值类型
-  interface ProgressUpdateCallbackResult {
-    /** 压缩进度，取值范围0 - 100，每秒有变化时更新 */
+  interface OnProgressUpdateCallbackOptions {
+    /**
+     * 压缩进度，取值范围0 - 100，每秒有变化时更新
+     */
     progress: number;
   }
 
-  // 定义压缩视频任务成功回调函数的返回值类型
-  interface CompressVideoSuccessResult {
-    /** 压缩后视频文件地址 */
+  type OnProgressUpdateCallback = (data: OnProgressUpdateCallbackOptions) => void;
+
+  interface CompressVideoSuccessOptions {
+    /**
+     * 压缩后视频文件地址
+     */
     uri: string;
-    /** 视频文件名称 */
+    /**
+     * 视频文件名称
+     */
     name: string;
-    /** 视频文件大小，单位B */
+    /**
+     * 视频文件大小，单位B
+     */
     size: string;
   }
 
-  // 定义获取视频信息成功回调函数的返回值类型
-  interface GetVideoInfoSuccessResult {
-    /** 视频文件地址 */
+  interface CompressVideoOptions {
+    /**
+     * 成功回调
+     */
+    success?: (data: CompressVideoSuccessOptions) => void;
+    /**
+     * 失败回调
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |200|接口功能异常，可能原因：压缩任务已执行过 abort、系统版本过低(Android L 或以上才支持)|
+     * |202|参数错误，可能原因：uri 不合法、所设宽高为奇数、所设码率已高于原码率、所设参数过低或过高超出设备支持范围|
+     * |203|找不到任务实例|
+     * |205|已发起了任务执行，无需再调用第二次|
+     * |300|IO 错误|
+     * |1001|原视频无效，可能原因：传来的文件不是视频|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface AbortOptions {
+    /**
+     * 成功回调
+     */
+    success?: () => void;
+    /**
+     * 失败回调
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |200|该任务已经执行完成或已被中断|
+     * |203|找不到任务实例|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetVideoInfoSuccessOptions {
+    /**
+     * 视频文件地址
+     */
     uri: string;
-    /** 视频文件名称 */
+    /**
+     * 视频文件名称
+     */
     name: string;
-    /** 视频文件大小，单位B */
+    /**
+     * 视频文件大小，单位B
+     */
     size: string;
-    /** 视频高度，单位像素 */
+    /**
+     * 视频高度，单位像素
+     */
     height: number;
-    /** 视频宽度，单位像素 */
+    /**
+     * 视频宽度，单位像素
+     */
     width: number;
-    /** 视频码率，单位bps(比特/秒) */
+    /**
+     * 视频码率，单位bps(比特/秒)
+     */
     bitrate: number;
-    /** 视频帧率，单位fps(帧/秒)。部分机型可能返回为空 */
+    /**
+     * 视频帧率，单位fps(帧/秒)。部分机型可能返回为空
+     */
     framerate: number | null;
   }
 
-  // 定义获取视频缩略图成功回调函数的返回值类型
-  interface GetVideoThumbnailSuccessResult {
-    /** 缩略图文件地址 */
+  interface GetVideoInfoOptions {
+    /**
+     * 视频文件地址
+     */
+    uri: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: GetVideoInfoSuccessOptions) => void;
+    /**
+     * 失败回调
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |200|接口功能异常|
+     * |202|参数错误|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetVideoThumbnailSuccessOptions {
+    /**
+     * 缩略图文件地址
+     */
     uri: string;
   }
 
-  // 定义视频处理接口类型
-  export default class Video {
+  interface GetVideoThumbnailOptions {
     /**
-     * 创建一个视频压缩任务实例
-     * @param options - 创建视频压缩任务实例的参数
-     * @returns 返回一个Video对象
+     * 视频文件地址
      */
+    uri: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: GetVideoThumbnailSuccessOptions) => void;
+    /**
+     * 失败回调
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |200|接口功能异常|
+     * |202|参数错误|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  /**
+   * 创建一个视频压缩任务实例
+   */
+  export default class Video {
     constructor(options: VideoCreationOptions);
 
     /**
      * 对压缩任务注册进度监听
-     * @param callback - 压缩进度变化时触发的回调函数，频率为每秒一次
      */
-    onprogressupdate: (data: ProgressUpdateCallbackResult) => void;
+    set onprogressupdate(callback: OnProgressUpdateCallback);
 
     /**
      * 执行压缩视频任务
-     * @param options - 包含成功回调、失败回调和执行结束后的回调的对象
      */
-    compressVideo(options: {
-      success?: (data: CompressVideoSuccessResult) => void;
-      fail?: (data: any, code: number) => void;
-      complete?: () => void;
-    }): void;
+    compressVideo(obj: CompressVideoOptions): void;
 
     /**
      * 放弃执行该压缩任务，若该压缩任务正在进行，则中断压缩进程
-     * @param options - 包含成功回调、失败回调和执行结束后的回调的对象
      */
-    abort(options: {
-      success?: () => void;
-      fail?: (data: any, code: number) => void;
-      complete?: () => void;
-    }): void;
+    abort(options: AbortOptions): void;
 
     /**
      * 获取视频信息
-     * @param options - 包含视频文件地址、成功回调、失败回调和执行结束后的回调的对象
      */
-    static getVideoInfo(options: {
-      uri: string;
-      success?: (data: GetVideoInfoSuccessResult) => void;
-      fail?: (data: any, code: number) => void;
-      complete?: () => void;
-    }): void;
+    static getVideoInfo(options: GetVideoInfoOptions): void;
 
     /**
      * 获取视频缩略图
-     * @param options - 包含视频文件地址、成功回调、失败回调和执行结束后的回调的对象
      */
-    static getVideoThumbnail(options: {
-      uri: string;
-      success?: (data: GetVideoThumbnailSuccessResult) => void;
-      fail?: (data: any, code: number) => void;
-      complete?: () => void;
-    }): void;
+    static getVideoThumbnail(options: GetVideoThumbnailOptions): void;
   }
 }
