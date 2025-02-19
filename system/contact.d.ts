cdcb139e07814918aafd8da09abfd26ecc296780
qinjiapeng <759848795@qq.com>
2025-02-19 10:40:52 +0800

    update @system.contact

--- system/contact.d.ts
+++ system/contact.d.ts
@@ -3,61 +3,22 @@
  * 接口声明: {"name": "system.contact"}
  */
 declare module '@system.contact' {
+  interface PickOptions {
     /**
-     * 选择联系人
-     * @param obj
+     * 成功回调
      */
-    function pick(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 联系人名称
-             */
-            displayName: string;
-            /**
-             * 电话号码
-             */
-            number: string;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
-
+    success?: (data: ContactItem) => void;
     /**
-     * 获取通讯录所有联系人列表，每次获取都需要用户授权
-     * [1050+]
-     * @param obj
+     * 失败回调
      */
-    function list(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 联系人列表
-             */
-            contactList: Contact[];
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
-}
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
 
-interface Contact {
+  interface ContactItem {
     /**
      * 联系人名称
      */
@@ -66,4 +27,38 @@ interface Contact {
      * 电话号码
      */
     number: string;
+  }
+
+  /**
+   * 选择联系人
+   */
+  function pick(obj: PickOptions): void;
+
+  interface ListOptions {
+    /**
+     * 成功回调
+     */
+    success?: (data: ListSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface ListSuccessOptions {
+    /**
+     * 联系人列表
+     */
+    contactList: ContactItem[];
+  }
+
+  /**
+   * 获取通讯录所有联系人列表，每次获取都需要用户授权
+   * [1050+]
+   */
+  function list(obj: ListOptions): void;
 }
