e40c4c7633a649750324617618f67f548a477d94
qinjiapeng <759848795@qq.com>
2025-02-19 10:37:14 +0800

    update @system.package

--- system/package.d.ts
+++ system/package.d.ts
@@ -3,122 +3,133 @@
  * 接口声明: {"name": "system.package"}
  */
 declare module '@system.package' {
+  interface HasInstalledOptions {
     /**
-     * 检测应用是否存在。支持检测原生应用是否已安装
-     * @param obj
-     *  package 应用包名
+     * 应用包名
      */
-    function hasInstalled(obj: {
-        /**
-         * 应用包名
-         */
-        package: string;
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 应用是否存在
-             */
-            result: boolean;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    package: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: HasInstalledSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
 
+  interface HasInstalledSuccessOptions {
     /**
-     * 安装应用, 支持安装原生应用
-     * @param obj
+     * 应用是否存在
      */
-    function install(obj: {
-        /**
-         * 应用包名
-         */
-        package: string;
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 是否成功发起安装操作
-             */
-            result: boolean;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    result: boolean;
+  }
 
-    /**
-     * 获取应用版本号、版本名称信息，包括原生应用和快应用
-     * [1070+]
-     * @param obj
-     */
-    function getInfo(obj: {
-        /**
-         * 应用包名
-         */
-        package: string;
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 版本号
-             */
-            versionCode: number;
-            /**
-             * 版本名称
-             */
-            versionName: string;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+  /**
+   * 检测应用是否存在。支持检测原生应用是否已安装
+   */
+  function hasInstalled(obj: HasInstalledOptions): void;
 
+  interface InstallOptions {
     /**
-     * 获取应用签名摘要信息，包括原生应用和快应用
-     * [1070+]
-     * @param obj
+     * 应用包名
      */
-    function getSignatureDigests(obj: {
-        /**
-         * 应用包名
-         */
-        package: string;
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 签名摘要信息列表，使用SHA-256
-             */
-            signatureDigests: [];
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    package: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: InstallSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface InstallSuccessOptions {
+    /**
+     * 是否成功发起安装操作
+     */
+    result: boolean;
+  }
+
+  /**
+   * 安装应用, 支持安装原生应用
+   */
+  function install(obj: InstallOptions): void;
+
+  interface GetInfoOptions {
+    /**
+     * 应用包名
+     */
+    package: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: GetInfoSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetInfoSuccessOptions {
+    /**
+     * 版本号
+     */
+    versionCode: number;
+    /**
+     * 版本名称
+     */
+    versionName: string;
+  }
+
+  /**
+   * 获取应用版本号、版本名称信息，包括原生应用和快应用
+   * [1070+]
+   */
+  function getInfo(obj: GetInfoOptions): void;
+
+  interface GetSignatureDigestsOptions {
+    /**
+     * 应用包名
+     */
+    package: string;
+    /**
+     * 成功回调
+     */
+    success?: (data: GetSignatureDigestsSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetSignatureDigestsSuccessOptions {
+    /**
+     * 签名摘要信息列表，使用SHA-256
+     */
+    signatureDigests: string[];
+  }
+
+  /**
+   * 获取应用签名摘要信息，包括原生应用和快应用
+   * [1070+]
+   */
+  function getSignatureDigests(obj: GetSignatureDigestsOptions): void;
 }
