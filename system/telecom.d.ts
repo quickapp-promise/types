24b8a6ba4737ac5ef4f7e25563c945d851c18439
qinjiapeng <759848795@qq.com>
2025-02-19 09:49:41 +0800

    update @system.telecom

--- system/telecom.d.ts
+++ system/telecom.d.ts
@@ -3,31 +3,25 @@
  * 接口声明: { "name": "system.telecom" }
  */
 declare module '@system.telecom' {
+    interface GetTelecomInfoOptions {
+        success?: (data: GetTelecomInfoSuccessOptions) => void;
+        fail?: (data: any, code: number) => void;
+        complete?: () => void;
+    }
+
+    interface GetTelecomInfoSuccessOptions {
+        /**
+         * 是否是5G设备
+         */
+        is5GDevice: boolean;
+        /**
+         * 5G开关是否已打开
+         */
+        is5GSwitchOpened: boolean;
+    }
+
     /**
      * 获取设备通信信息
-     * @param obj
      */
-    function getTelecomInfo(obj: {
-        /**
-         * 成功回调
-         */
-        success: (data: {
-            /**
-             * 是否是5G设备
-             */
-            is5GDevice: boolean;
-            /**
-             * 5G开关是否已打开
-             */
-            is5GSwitchOpened: boolean;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    function getTelecomInfo(obj: GetTelecomInfoOptions): void;
 }
