7734d9c899e6f2373acb40ff0f5ef4bf7a698a30
qinjiapeng <759848795@qq.com>
2025-02-19 17:50:34 +0800

    update @service.health

--- service/health.d.ts
+++ service/health.d.ts
@@ -3,86 +3,98 @@
  * 接口声明: { "name": "service.health" }
  */
 declare module '@service.health' {
+  interface HasStepsOfDayOptions {
     /**
-     * 是否支持提供每日步数的功能
-     * @param obj
+     * 成功回调
      */
-    function hasStepsOfDay(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 是否支持提供每日步数的功能.true 支持，false 不支持
-             */
-            support: boolean;
-        }) => void;
-        /**
-         * 失败回调，返回失败原因
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
-
+    success?: (data: HasStepsOfDaySuccessOptions) => void;
     /**
-     * 获取每个自然日的步数，返回的是调用接口时，用户今天已经累计的步数
-     * [权限说明:需要用户授权才能获取数据]
-     * @param obj
+     * 失败回调
      */
-    function getTodaySteps(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 返回当天步数
-             */
-            steps: number;
-        }) => void;
-        /**
-         * 失败回调，返回失败原因
-         * 201: 用户拒绝授权
-         * 1001: 还不支持获取步数
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
-
+    fail?: (data: any, code: number) => void;
     /**
-     * 获取最近七个自然日每天的步数，包括今天。需要用户授权才能获取数据。
-     * [权限说明:需要用户授权才能获取数据]
-     * @param obj
+     * 执行结束后的回调
      */
-    function getLastWeekSteps(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 返回当天步数
-             */
-            stepsList: Steps[];
-        }) => void;
-        /**
-         * 失败回调，返回失败原因
-         * 201: 用户拒绝授权
-         * 1001: 还不支持获取步数
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
-}
+    complete?: () => void;
+  }
 
-interface Steps {
+  interface HasStepsOfDaySuccessOptions {
+    /**
+     * 是否支持提供每日步数的功能.true 支持，false 不支持
+     */
+    support: boolean;
+  }
+
+  /**
+   * 是否支持提供每日步数的功能
+   */
+  function hasStepsOfDay(obj: HasStepsOfDayOptions): void;
+
+  interface GetTodayStepsOptions {
+    /**
+     * 成功回调
+     */
+    success?: (data: GetTodayStepsSuccessOptions) => void;
+    /**
+     * 失败回调
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |201|用户拒绝授权|
+     * |207|用户拒绝并勾选不再询问复选框 [1100+]|
+     * |1001|还不支持获取步数|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetTodayStepsSuccessOptions {
+    /**
+     * 返回当天步数
+     */
+    steps: number;
+  }
+
+  /**
+   * 获取每个自然日的步数，返回的是调用接口时，用户今天已经累计的步数
+   * @description
+   * 权限说明
+   * - 需要用户授权才能获取数据
+   */
+  function getTodaySteps(obj: GetTodayStepsOptions): void;
+
+  interface GetLastWeekStepsOptions {
+    /**
+     * 成功回调
+     */
+    success?: (data: GetLastWeekStepsSuccessOptions) => void;
+    /**
+     * 失败回调，返回失败原因
+     * @description
+     * |错误码|说明|
+     * |---|---|
+     * |201|用户拒绝授权|
+     * |207|用户拒绝并勾选不再询问复选框 [1100+]|
+     * |1001|还不支持获取步数|
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  interface GetLastWeekStepsSuccessOptions {
+    /**
+     * 最近七天步数列表数据
+     */
+    stepsList: DateSteps[];
+  }
+
+  interface DateSteps {
     /**
      * 日期(2019-04-08)
      */
@@ -91,4 +103,13 @@ interface Steps {
      * 日期对应的步数
      */
     steps: number;
+  }
+
+  /**
+   * 获取最近七个自然日每天的步数，包括今天。需要用户授权才能获取数据。
+   * @description
+   * 权限说明
+   * - 需要用户授权才能获取数据
+   */
+  function getLastWeekSteps(obj: GetLastWeekStepsOptions): void;
 }
