734830335880b5a2c689b7123fcfd1cc3e94b03d
qinjiapeng <759848795@qq.com>
2025-02-19 10:03:06 +0800

    update @system.volume

--- system/volume.d.ts
+++ system/volume.d.ts
@@ -3,50 +3,54 @@
  * 接口声明: {"name": "system.volume"}
  */
 declare module '@system.volume' {
+  interface GetMediaValueOptions {
     /**
-     * 获取当前多媒体音量
-     * @param obj
+     * 成功回调
      */
-    function getMediaValue(obj: {
-        /**
-         * 成功回调
-         */
-        success?: (data: {
-            /**
-             * 系统媒体当前音量，0.0-1.0 之间
-             */
-            value: number;
-        }) => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    success?: (data: GetMediaValueSuccessOptions) => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
 
+  interface GetMediaValueSuccessOptions {
     /**
-     * 获取当前多媒体音量
-     * @param obj
+     * 系统媒体当前音量，0.0-1.0 之间
      */
-    function setMediaValue(obj: {
-        /**
-         * 设置的音量，0.0-1.0之间
-         */
-        value: number;
-        /**
-         * 成功回调
-         */
-        success?: () => void;
-        /**
-         * 失败回调
-         */
-        fail?: (data: any, code: number) => void;
-        /**
-         * 执行结束后的回调
-         */
-        complete?: () => void;
-    }): void;
+    value: number;
+  }
+
+  /**
+   * 获取当前多媒体音量
+   */
+  function getMediaValue(obj: GetMediaValueOptions): void;
+
+  interface SetMediaValueOptions {
+    /**
+     * 设置的音量，0.0-1.0之间
+     */
+    value: number;
+    /**
+     * 成功回调
+     */
+    success?: () => void;
+    /**
+     * 失败回调
+     */
+    fail?: (data: any, code: number) => void;
+    /**
+     * 执行结束后的回调
+     */
+    complete?: () => void;
+  }
+
+  /**
+   * 获取当前多媒体音量
+   */
+  function setMediaValue(obj: SetMediaValueOptions): void;
 }
