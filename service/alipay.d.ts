9554ade070e6457958711b7f9c455343d6db96e0
qinjiapeng <759848795@qq.com>
2025-02-19 19:02:56 +0800

    update @service.alipay

--- service/alipay.d.ts
+++ service/alipay.d.ts
@@ -3,19 +3,20 @@
  * 接口声明: {"name": "service.alipay"}
  */
 declare module '@service.alipay' {
+  interface PayOptions {
     /**
-     * 使用支付宝支完成支付
-     * @param obj
+     * 服务端生成的订单信息
      */
-    function pay(obj: {
-        /**
-         * 服务端生成的订单信息
-         */
-        orderInfo: string;
-        /**
-         * 支付结果回调，格式参考支付宝的通知参数说明文档
-         * [详见:https://docs.open.alipay.com/204/105302 ]
-         */
-        callback?: (ret: any) => void;
-    }): void;
+    orderInfo: string;
+    /**
+     * 支付结果回调，格式参考支付宝的通知参数说明文档
+     * [详见:https://docs.open.alipay.com/204/105302 ]
+     */
+    callback?: (ret: any) => void;
+  }
+
+  /**
+   * 使用支付宝支完成支付
+   */
+  function pay(obj: PayOptions): void;
 }
