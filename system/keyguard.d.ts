ff2f5c8a36a597dae70b4ecade8bccfc293a5895
qinjiapeng <759848795@qq.com>
2025-02-19 12:41:34 +0800

    update @system.keyguard

--- system/keyguard.d.ts
+++ system/keyguard.d.ts
@@ -1,26 +1,29 @@
 /**
- * 1090+
+ * 锁屏 keyguard 1090+
  * 实现系统锁屏的相关功能
  * 接口声明: { "name": "system.keyguard" }
  */
 declare module '@system.keyguard' {
+  interface GetKeyguardLockedStatusOptions {
     /**
-     * 获取快应用当前环境是否为锁屏状态
-     * @param obj
+     * 成功回调，返回值为一个对象
      */
-    function getKeyguardLockedStatus(obj: {
-        /**
-         * 成功回调，返回值为一个对象
-         */
-        success?: (data: {
-            /**
-             * true 当前快应用在锁屏状态；false 当前快应用在非锁屏状态
-             */
-            isKeyguardLocked: boolean;
-        }) => void;
-        /**
-         * 失败的回调函数
-         */
-        fail?: (data: string, code: number) => void;
-    }): void;
+    success?: (data: GetKeyguardLockedStatusSuccessOptions) => void;
+    /**
+     * 失败的回调函数
+     */
+    fail?: (data: string, code: number) => void;
+  }
+
+  interface GetKeyguardLockedStatusSuccessOptions {
+    /**
+     * true 当前快应用在锁屏状态；false 当前快应用在非锁屏状态
+     */
+    isKeyguardLocked: boolean;
+  }
+
+  /**
+   * 获取快应用当前环境是否为锁屏状态
+   */
+  function getKeyguardLockedStatus(obj: GetKeyguardLockedStatusOptions): void;
 }
